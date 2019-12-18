7429e8cb73a6781a13dc51637adb1f07031da312
danmaiJJ <lidan9152@163.com>
2019-12-18 10:01:40 +0800

    函数装饰器

--- test201912/test1216/decorator.py
+++ test201912/test1216/decorator.py
@@ -1,7 +1,11 @@
 import time
+from functools import wraps
 
 def timeDecorator(func):
+    '''timeDecorator的注释'''
+    @wraps(func)
     def wrapper():
+        '''wrapper的注释'''
         star = time.time()
         f = func()
         end = time.time()
@@ -11,14 +15,19 @@ def timeDecorator(func):
 
 @timeDecorator
 def methodA():
+    '''methodA的注释'''
     print('1、第一个函数')
     time.sleep(0.8)
 
 @timeDecorator
 def methodB():
+    '''methodB的注释'''
     print('2、第二个函数')
     time.sleep(0.5)
 
 if __name__ == '__main__':
     methodA()
-    methodB()
+    # methodB()
+    print(methodA.__name__)
+    print(methodA.__doc__)
+
